bbc034aae2b4c72bcbbbf556cdf54405537536a4
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2024-09-03 14:01:37 +0300

    Initial Commit

---
+++ simulate-not-responding/simulate_not_respondingApp.swift
@@ -0,0 +1,17 @@
+//
+//  simulate_not_respondingApp.swift
+//  simulate-not-responding
+//
+//  Created by Raz Luvaton on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct simulate_not_respondingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
